cbfe84bef9cec995be6afb216c2e92291b818981
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-06-10 17:35:17 +0100

    Get rid of zebrafish base model

--- fishdb/models/fish.py
+++ fishdb/models/fish.py
@@ -1,14 +1,65 @@
-# from .zebrafish_queryset import FishQuerySet
-# from .zebrafish_permission import ZebrafishPermission
-from .zebrafish_base import AbstractFish, Fish
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from model_utils import Choices
 
 
-# class Fish(AbstractFish):
+class AbstractFish(models.Model):
+    """
+    Must be compatible with Congento model scheme!
+    """
 
-#     objects = FishQuerySet.as_manager()
+    LINES = Choices(
+        ("wt", "WT"),
+        ("tg", "Tg"),
+        ("mu", "Mutant"),
+        ("cko", "CRISPR KO"),
+        ("cki", "CRISPR KI"),
+        ("other", "Other"),
+    )
 
-#     def save(self, *args, **kwargs):
-#         super().save(*args, **kwargs)
+    AVAILABILITIES = Choices(
+        ("live", "Live"),
+        ("cryo", "Cryopreserved"),
+        ("both", "Live & Cryopreserved"),
+        ("none", "Unavailable"),
+    )
 
-#         if self.lab is not None:
-#             ZebrafishPermission.objects.get_or_create(zebrafish=self, group=self.lab, viewonly=False)
+    created = models.DateTimeField("Created", auto_now_add=True)
+    modified = models.DateTimeField("Updated", auto_now=True)
+
+    species = models.ForeignKey(to='fishdb.Species', on_delete=models.PROTECT, related_name='fish')
+    background = models.CharField(max_length=20)
+    genotype = models.CharField(max_length=20)
+    phenotype = models.CharField(max_length=20)
+    origin = models.CharField(max_length=20)
+
+    # Fields shared with other congento animal models
+    availability = models.CharField(max_length=4, choices=AVAILABILITIES)
+    comments = models.TextField(blank=True)
+    link = models.URLField(blank=True)
+    mta = models.BooleanField(verbose_name="MTA", default=False)
+
+    line_name = models.CharField(max_length=20)
+    line_number = models.CharField(max_length=20)
+    line_type = models.CharField(max_length=5, choices=LINES)
+    line_type_other = models.CharField(max_length=20, verbose_name="", blank=True)
+
+    class Meta:
+        verbose_name = "fish"
+        verbose_name_plural = "fish"
+        abstract = True
+
+    def __str__(self):
+        return self.line_name
+
+    def clean(self):
+        if self.line_type == self.LINES.other:
+            if not self.line_type_other:
+                raise ValidationError({"line_type_other": "This field is required."})
+        else:
+            self.line_type_other = ""
+
+
+class Fish(AbstractFish):
+    public = models.BooleanField("Public", default=False)
