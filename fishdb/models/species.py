44c4d93d65737d85c10b8af2885a8b34488f3de3
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-12 17:31:01 +0100

    Create abstract model for Fish species

--- fishdb/models/species.py
+++ fishdb/models/species.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 
-class Species(models.Model):
+class AbstractSpecies(models.Model):
     name = models.CharField(max_length=80)
 
     class Meta:
         verbose_name = "species"
         verbose_name_plural = "species"
         ordering = ["name"]
+        abstract = True
 
     def __str__(self):
         return self.name
+
+
+class Species(AbstractSpecies):
+    ...
