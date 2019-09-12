ef24bea0c4d51d8d3baba16792ddb8f823ca5d25
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-12 15:58:23 +0100

    Create abstract model for Fish category

--- fishdb/models/category.py
+++ fishdb/models/category.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 
-class Category(models.Model):
+class AbstractCategory(models.Model):
     name = models.CharField(max_length=40)
 
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
         ordering = ["name"]
+        abstract = True
 
     def __str__(self):
         return self.name
+
+
+class Category(AbstractCategory):
+    ...
