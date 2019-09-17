68d641c87c116a57fde409c642c9794aca0c8560
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-09-17 17:35:51 +0100

    Get rid of AbstractFish

--- fishdb/models/fish.py
+++ fishdb/models/fish.py
@@ -1,11 +1,62 @@
 from django.conf import settings
 from django.db import models
+from model_utils import Choices
 
 from fishdb.querysets import FishQuerySet
-from fishdb.base_fish import AbstractFish
 
 
-class Fish(AbstractFish):
+class Fish(models.Model):
+
+    # =========================================================================
+    # WARNING!
+    # The following section contain the fields supported by Congento.
+    # These should not be modified or else synchronization with Congento
+    # database may fail.
+    # =========================================================================
+
+    AVAILABILITIES = Choices(
+        ("live", "Live"),
+        ("cryo", "Cryopreserved"),
+        ("both", "Live & Cryopreserved"),
+        ("none", "Unavailable"),
+    )
+
+    # Fields shared with other congento animal models
+    created = models.DateTimeField("Created", auto_now_add=True)
+    modified = models.DateTimeField("Updated", auto_now=True)
+    availability = models.CharField(max_length=4, choices=AVAILABILITIES)
+    link = models.URLField(blank=True)
+
+    # Specific fields for this animal model
+    strain_name = models.CharField(max_length=255)
+    common_name = models.CharField(max_length=50, blank=True)
+    species = models.ForeignKey(
+        to="fishdb.Species",
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)s_related",
+    )
+    category = models.ForeignKey(
+        to="fishdb.Category",
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)s_related",
+    )
+    background = models.CharField(max_length=30)
+    genotype = models.CharField(max_length=30)
+    phenotype = models.CharField(max_length=30)
+    origin = models.CharField(
+        verbose_name="Imported from",
+        max_length=80,
+        blank=True,
+        help_text="Leave blank for in-house generated lines",
+    )
+    quarantine = models.BooleanField(verbose_name="Quarantine", default=False)
+    mta = models.BooleanField(verbose_name="MTA", default=True)
+    line_description = models.TextField(blank=True)
+
+    # =========================================================================
+    # The following section contain specific fields required by the Fish DB.
+    # =========================================================================
+
     public = models.BooleanField(verbose_name="Public through Congento", default=False)
 
     # TODO test and fix the two ownership fields
@@ -21,3 +72,16 @@ class Fish(AbstractFish):
     comments = models.TextField(blank=True)
 
     objects = FishQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = "fish"
+        verbose_name_plural = "fish"
+
+    def __str__(self):
+        return self.strain_name
+
+    def get_origin(self):
+        # FIXME get correct isntitution name after from configuration (settings or DB)
+        return self.origin or getattr(settings, "INSTITUTION_NAME", "in-house")
+
+    get_origin.short_description = "Origin"
