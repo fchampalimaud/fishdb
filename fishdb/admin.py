04db8cdff18d56c4c7cf9ad873117f41611c8366
Luís Teixeira <micboucinha@gmail.com>
2019-11-29 17:33:47 +0000

    Add FishResource to FishAdmin

--- fishdb/admin.py
+++ fishdb/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from import_export import resources, widgets
+from import_export.admin import ExportActionMixin, ImportMixin
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
 
 from . import models
+from .models import Fish, Category, Species
 
 
-@admin.register(models.Fish)
-class FishAdmin(admin.ModelAdmin):
+class FishResource(resources.ModelResource):
+    category = Field(attribute='category', column_name='category', widget=ForeignKeyWidget(Category, 'name'))
+    species = Field(attribute='species', column_name='species', widget=ForeignKeyWidget(Species, 'name'))
+    class Meta:
+        model = Fish
+        skip_unchanged = True
+
+class FishAdmin(ImportMixin, ExportActionMixin, admin.ModelAdmin):
+    resource_class = FishResource
     readonly_fields = ["created", "modified"]
 
-
+admin.site.register(Fish, FishAdmin)
 admin.site.register(models.Species)
 admin.site.register(models.Category)
 admin.site.register(models.Location)
