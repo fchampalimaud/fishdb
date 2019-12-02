762fd47f43da063a36a78b1c6b5d3cd7eafc1178
Luís Teixeira <micboucinha@gmail.com>
2019-12-02 17:51:41 +0000

    Remove redundant option since it is the default

--- fishdb/admin.py
+++ fishdb/admin.py
@@ -11,10 +11,10 @@ from .models import Fish, Category, Species
 class FishResource(resources.ModelResource):
     category = Field(attribute='category', column_name='category', widget=ForeignKeyWidget(Category, 'name'))
     species = Field(attribute='species', column_name='species', widget=ForeignKeyWidget(Species, 'name'))
+    #TODO maintainer and ownership FKs missing
     class Meta:
         model = Fish
         skip_unchanged = True
-        report_skipped = True
         clean_model_instances = True
 
 class FishAdmin(ImportMixin, ExportActionMixin, admin.ModelAdmin):
