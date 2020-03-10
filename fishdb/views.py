5db47034ef5880e2f67da15706bdf9353e2357e4
Luís Teixeira <micboucinha@gmail.com>
2020-03-10 17:15:49 +0000

    Update Fish template generation to not depend on django-import-export

--- fishdb/views.py
+++ fishdb/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from fishdb.admin import FishResource
 import csv
 from django.http import HttpResponse
+from .models import Fish
 
 # Create your views here.
 @login_required
 def get_fish_template(request):
-    fish_resource = FishResource()
-    dataset = fish_resource.export()
-    
+    fields = [item.name for item in Fish()._meta.get_fields()]
+
+    ignore_export = ['_state', 'id', 'created', 'modified']
+    header_list = ['species', 'category', 'strain_name', 'common_name', 'line_number', 'background', 
+                   'genotype', 'phenotype', 'origin',
+                   'availability', 'link', 'quarantine', 'mta', 'public', 'line_description', 'comments',
+                   'maintainer', 'ownership']
+
+    # add those fields that are not in either list to the end of the header_list
+    for item in fields:
+        if item not in ignore_export and item not in header_list:
+            header_list.append(item)
+
     # prepare response
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="fish_template.csv"'
 
-    if dataset.csv:
-        header = dataset.csv.splitlines()
-        if len(header) > 0:
-            csv_list = header[0].split(',')
-            writer = csv.writer(response)
-            writer.writerow(csv_list)
+    writer = csv.writer(response)
+    writer.writerow(header_list)
 
     return response
