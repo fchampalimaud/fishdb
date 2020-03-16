edbfb214b92569b590105b52aecd9111424f2816
Luís Teixeira <micboucinha@gmail.com>
2020-03-16 13:34:54 +0000

    Fix importing problem with validation errors

--- fishdb/apps/fish.py
+++ fishdb/apps/fish.py
@@ -69,7 +69,7 @@ class FishImportWidget(BaseWidget):
 
             # Test the import first
             result = fish_resource.import_data(dataset, dry_run=True, use_transactions=True, collect_failed_rows=True)
-            if result.has_errors():
+            if result.has_errors() or result.has_validation_errors():
                 import itertools
                 MAX_ERRORS_SHOWN = 3
                 val_errors = ""
